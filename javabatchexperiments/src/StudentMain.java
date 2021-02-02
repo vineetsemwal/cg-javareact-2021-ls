1c4a0fe286672b211cc6b35db345095370510660
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-02 17:01:15 +0530

    student project improved

--- javabatchexperiments/src/StudentMain.java
+++ javabatchexperiments/src/StudentMain.java
@@ -2,6 +2,13 @@
 public class StudentMain {
 
 	public static void main(String args[]) {
+        StudentMain project=new StudentMain();
+        project.start();
+				
+	}
+	
+	public void start() {
+		Student students[]=new Student[2];
 		Student student1 = new Student();
 		student1.id = 1;
 		student1.name = "abhishek";
@@ -11,14 +18,21 @@ public class StudentMain {
 		student2.id = 2;
 		student2.name = "suriya";
 		student2.score = 50;
+		
+		students[0]=student1;
+		students[1]=student2;
+		
+		for(int i=0;i<students.length;i++) {
+			Student iteratedStudent=students[i];
+			displayStudent(iteratedStudent);			
+		}
 
-		displayStudent(student1);
-		displayStudent(student2);
 	}
 
-	static void displayStudent(Student student) {
+	void displayStudent(Student student) {
 		String feedback = student.performance();
-		System.out.println("student " + student.id + " " + student.name + " " + student.score + " " + feedback);
+		System.out.println("student " + student.id + " " + student.name + " " + 
+		student.score + " " + feedback);
 
 	}
 
