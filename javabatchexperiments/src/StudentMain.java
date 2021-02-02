0204a529c6e001a84dfbdac8c06497ba07860f27
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-02 16:48:43 +0530

    more changes

--- javabatchexperiments/src/StudentMain.java
+++ javabatchexperiments/src/StudentMain.java
@@ -2,13 +2,24 @@
 public class StudentMain {
 
 	public static void main(String args[]) {
-		Student student1=new Student();
-		student1.id=1;
-		student1.name="abhishek";
-		student1.score=70;
-		String feedback1=student1.performance();
-		System.out.println("student1 "+student1.id+" "+
-		student1.name+" "+student1.score+" "+feedback1);
+		Student student1 = new Student();
+		student1.id = 1;
+		student1.name = "abhishek";
+		student1.score = 70;
+
+		Student student2 = new Student();
+		student2.id = 2;
+		student2.name = "suriya";
+		student2.score = 50;
+
+		displayStudent(student1);
+		displayStudent(student2);
 	}
-	
+
+	static void displayStudent(Student student) {
+		String feedback = student.performance();
+		System.out.println("student " + student.id + " " + student.name + " " + student.score + " " + feedback);
+
+	}
+
 }
