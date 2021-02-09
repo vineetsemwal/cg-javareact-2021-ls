23a3055fbe450964a751444a7d6ddca142bc2d42
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-09 10:20:41 +0530

    productmain improved

--- javabatchexperiments/src/exceptionex/ProductMain.java
+++ javabatchexperiments/src/exceptionex/ProductMain.java
@@ -5,20 +5,27 @@ public class ProductMain {
 
     public static void main(String[] args) {
         ProductMain app = new ProductMain();
-        Product product =null;
-        app.displayUsed(product);
+        int productIdInput=123;//
+        Product product = app.fetchProductFromServer(productIdInput);
+        app.display(product);
         System.out.println("bye, see you");
 
     }
 
-    void displayUsed(Product product){
-        display(product);
+    Product fetchProductFromServer(int productID){
+        Product product = new Product(productID,"samsung");
+        return product;
     }
 
     void display(Product product) {
+        try {
             System.out.println("inside display, before product details are to be displayed");
             System.out.println("product=" + product.getId() + " -" + product.getName());
             System.out.println("product details are displayed");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("sorry we couldn't display product details right now, check if your productid is correct");
+        }
     }
 
 
