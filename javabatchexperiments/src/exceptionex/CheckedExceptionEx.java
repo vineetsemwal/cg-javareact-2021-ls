7770fafcce2e7c9cb280d817d19666fca182490a
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-09 12:51:30 +0530

    throw and throws keyword used

--- javabatchexperiments/src/exceptionex/CheckedExceptionEx.java
+++ javabatchexperiments/src/exceptionex/CheckedExceptionEx.java
@@ -5,35 +5,46 @@ public class CheckedExceptionEx {
         CheckedExceptionEx example = new CheckedExceptionEx();
         int ageInput = 27;
         String nameInput = "";
-        example.isDrivingPermissible(ageInput, nameInput);
-
-    }
-
-    public void isDrivingPermissible(int age, String name) {
         try {
-            if (age < 0 || age > 120) {
-                InvalidAgeException ageException = new InvalidAgeException("age can't be " + age);
-                throw ageException;
-            }
-
-            if (name == null || name.isEmpty()) {
-                throw new InvalidNameException("name can't be null or empty");
-            }
-
-            if (age >= 18) {
-                System.out.println(name + ", you can drive");
-            } else {
-                System.out.println(name + ", you can't drive");
-            }
+            example.use(ageInput, nameInput);
         } catch (InvalidAgeException e) {
             System.out.println("sorry can't display information to you as age is invalid");
             String exceptionMsg = e.getMessage();
             System.out.println(exceptionMsg);
+            e.printStackTrace();
         } catch (InvalidNameException e) {
             System.out.println("sorry can't display information to you as name is invalid");
             String exceptionMsg = e.getMessage();
             System.out.println(exceptionMsg);
+            e.printStackTrace();
+        } catch (Throwable e) {
+            System.out.println("something went wrong");
+            e.printStackTrace();
         }
     }
+
+    public void use(int age, String name) throws InvalidAgeException, InvalidNameException {
+        isDrivingPermissible(age, name);
+
+    }
+
+    public void isDrivingPermissible(int age, String name)
+            throws InvalidNameException, InvalidAgeException {
+        if (age < 0 || age > 120) {
+            InvalidAgeException ageException = new InvalidAgeException("age can't be " + age);
+            throw ageException;
+        }
+
+        if (name == null || name.isEmpty()) {
+            throw new InvalidNameException("name can't be null or empty");
+        }
+
+        if (age >= 18) {
+            System.out.println(name + ", you can drive");
+        } else {
+            System.out.println(name + ", you can't drive");
+        }
+    }
+
 
 }
