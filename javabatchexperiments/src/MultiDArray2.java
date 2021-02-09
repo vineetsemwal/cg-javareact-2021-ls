b364331cef739a1aeeccb65df2d2bf1f37f9279a
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-09 15:58:35 +0530

    foreach multid added

--- javabatchexperiments/src/MultiDArray2.java
+++ javabatchexperiments/src/MultiDArray2.java
@@ -1,5 +1,3 @@
-package calculator;
-
 public class MultiDArray2 {
     public static void main(String[] args){
         int array2D[][]=new int[2][];
@@ -7,12 +5,21 @@ public class MultiDArray2 {
         int row1[]={10,11,12};
         array2D[0]=row0;
         array2D[1]=row1;
+        /*
         for (int row=0;row<array2D.length;row++){
             System.out.println();
             int columnArray[]=array2D[row];
             for(int col=0;col<columnArray.length;col++){
                 System.out.print(columnArray[col]+"  ");
             }
+        }*/
+
+        for(int row[]:array2D){
+            System.out.println();
+            for (int element:row){
+                System.out.print(element+" ");
+            }
         }
+
     }
 }
