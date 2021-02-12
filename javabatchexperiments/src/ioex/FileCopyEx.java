fcffa05f803edacbf7194523efb90a5c563ca06c
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-12 10:41:53 +0530

    filecopy example read byte version

--- javabatchexperiments/src/ioex/FileCopyEx.java
+++ javabatchexperiments/src/ioex/FileCopyEx.java
@@ -1,7 +1,6 @@
 package ioex;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 
 public class FileCopyEx {
     public static void main(String[] args) {
@@ -10,8 +9,51 @@ public class FileCopyEx {
     }
 
     void start() {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            File inputFile = new File("/home/vineet/work/fileapi/inputfile.txt");
+            File outFile = new File("/home/vineet/work/fileapi/outfile.txt");
+
+            inputStream = new FileInputStream(inputFile);
+            outputStream = new FileOutputStream(outFile);
+            int redByte = -1;
+            int bytesCount=0;
+            while ((redByte = inputStream.read()) !=-1) {
+                outputStream.write(redByte);
+                bytesCount++;
+            }
+            System.out.println("bytes written="+bytesCount);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            close(outputStream);
+            close(inputStream);
+        }
+    }
+
+    void close(InputStream inputStream) {
+        try {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    void close(OutputStream outputStream) {
+        try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
