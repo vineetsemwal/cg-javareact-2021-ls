7b37d0c17143a03d693bcd5019b835fd1461d0b0
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-12 11:40:27 +0530

    file buffer example added

--- javabatchexperiments/src/ioex/FileBufferEx.java
+++ javabatchexperiments/src/ioex/FileBufferEx.java
@@ -2,31 +2,35 @@ package ioex;
 
 import java.io.*;
 
-public class FileCopyFastEx {
-
+public class FileBufferEx {
     public static void main(String[] args){
-        FileCopyFastEx app=new FileCopyFastEx();
+        FileBufferEx app=new FileBufferEx();
         app.start();
     }
+
 
     void start() {
         InputStream inputStream = null;
         OutputStream outputStream = null;
+        BufferedInputStream bin=null;
+        BufferedOutputStream bout=null;
         try {
             File inputFile = new File("/home/vineet/work/fileapi/inputfile.txt");
             File outFile = new File("/home/vineet/work/fileapi/outfile.txt");
 
             inputStream = new FileInputStream(inputFile);
+            bin=new BufferedInputStream(inputStream);
             outputStream = new FileOutputStream(outFile);
-            //2500;2000+500
+            bout=new BufferedOutputStream(outputStream);
             int bytesCount=0;
-            byte redBytes[]=new byte[1000];
-            int newBytesRed;
-            while ((newBytesRed = inputStream.read(redBytes))>0 ) {
-                outputStream.write(redBytes,0,newBytesRed);
-                bytesCount=bytesCount+newBytesRed;//0+1000+1000+500
+            byte redBytes[]=new byte[100];
+           int newBytesRed;
+            while ((newBytesRed = bin.read(redBytes))>0 ) {
+                bout.write(redBytes,0,newBytesRed);
+                bytesCount=bytesCount+newBytesRed;
             }
             System.out.println("bytes written="+bytesCount);
+            bout.flush();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -34,7 +38,9 @@ public class FileCopyFastEx {
             e.printStackTrace();
         } finally {
             close(outputStream);
+            close(bout);
             close(inputStream);
+            close(bin);
         }
     }
 
