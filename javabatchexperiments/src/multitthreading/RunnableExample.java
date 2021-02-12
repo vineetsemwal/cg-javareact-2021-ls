0ae3f159d62044e516c6a2c66b98cb3ae96f47fd
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-12 16:49:52 +0530

    islive, join demo added

--- javabatchexperiments/src/multitthreading/RunnableExample.java
+++ javabatchexperiments/src/multitthreading/RunnableExample.java
@@ -2,13 +2,26 @@ package multitthreading;
 
 public class RunnableExample {
 
-    public static void main(String[] args){
+    public static void main(String[] args)throws InterruptedException{
         Thread thread1 = new Thread(new RunnableTask("thread1"));
         Thread thread2=new Thread(new RunnableTask("thread2"));
         Thread thread3=new Thread(new RunnableTask("thread3"));
         thread1.start();
         thread2.start();
         thread3.start();
+        boolean isT1Alvie=thread1.isAlive();
+        boolean isT2Alive= thread2.isAlive();
+        boolean isT3Alive=thread3.isAlive();
+        System.out.println( "t1 alive="+isT1Alvie+" t2 alive="+isT2Alive+" t3 alive "+isT3Alive);
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        isT1Alvie=thread1.isAlive();
+        isT2Alive= thread2.isAlive();
+        isT3Alive=thread3.isAlive();
+        System.out.println( "t1 alive="+isT1Alvie+" t2 alive="+isT2Alive+" t3 alive "+isT3Alive);
+
         System.out.println("main thread finished");
+
     }
 }
