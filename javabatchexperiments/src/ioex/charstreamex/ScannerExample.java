cdcbcc7be2b62f0342513e5dd5bbe5385a153ae0
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-12 17:07:30 +0530

    scanner demo added

---
+++ javabatchexperiments/src/ioex/charstreamex/ScannerExample.java
@@ -0,0 +1,16 @@
+package ioex.charstreamex;
+import java.io.InputStream;
+import java.util.*;
+
+public class ScannerExample {
+    public static void main(String args[]){
+        InputStream inputStream=System.in;
+        Scanner scanner = new Scanner(inputStream);
+        System.out.println( "provide name");
+        String name=scanner.next();
+        System.out.println("provide age");
+        int age=scanner.nextInt();
+        System.out.println("name is "+name+" age is "+age);
+        scanner.close();
+    }
+}
