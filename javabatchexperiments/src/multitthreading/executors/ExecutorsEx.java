6fca898b21fa41bf5799628888ae4b619b86429d
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-15 12:34:08 +0530

    singlethreadexecutor demo added

--- javabatchexperiments/src/multitthreading/executors/ExecutorsEx.java
+++ javabatchexperiments/src/multitthreading/executors/ExecutorsEx.java
@@ -7,8 +7,9 @@ import java.util.concurrent.Executors;
 
 public class ExecutorsEx {
     public static void main(String args[]) {
-       // ExecutorService executorService = Executors.newCachedThreadPool();
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        // ExecutorService executorService = Executors.newCachedThreadPool();
+        //ExecutorService executorService = Executors.newFixedThreadPool(2);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
         Runnable task1 = new RunnableTask("task1");
         Runnable task2 = new RunnableTask("task2");
         executorService.submit(task1);
