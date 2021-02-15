39d2c24f1d2d0a2cb7d8a9ded27a40e6b588476e
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-15 12:28:58 +0530

    fixed threadpool demo added

--- javabatchexperiments/src/multitthreading/executors/ExecutorsEx.java
+++ javabatchexperiments/src/multitthreading/executors/ExecutorsEx.java
@@ -7,7 +7,8 @@ import java.util.concurrent.Executors;
 
 public class ExecutorsEx {
     public static void main(String args[]) {
-        ExecutorService executorService = Executors.newCachedThreadPool();
+       // ExecutorService executorService = Executors.newCachedThreadPool();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         Runnable task1 = new RunnableTask("task1");
         Runnable task2 = new RunnableTask("task2");
         executorService.submit(task1);
