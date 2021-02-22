81f993ee475218c74ae2bf4d2c92567e6d449f5c
vineetsemwal <vineetsemwal82@gmail.com>
2021-02-22 10:38:37 +0530

    enum example added

---
+++ javabatchexperiments/src/enumex/EnumMain.java
@@ -0,0 +1,14 @@
+package enumex;
+
+public class EnumMain {
+
+    public static void main(String[] args){
+        WeekDay[] weekdays=WeekDay.values();
+        for (WeekDay weekday:weekdays){
+            System.out.print("weekday="+weekday +" position="+weekday.ordinal());
+            System.out.println(" start time="+weekday.getStartTime()+" endtime="+weekday.getEndTime());
+        }
+
+
+    }
+}
---
+++ javabatchexperiments/src/enumex/WeekDay.java
@@ -0,0 +1,27 @@
+package enumex;
+
+public enum WeekDay {
+
+    SUNDAY("9am","6pm"),
+    MONDAY("9:30am","6:30pm"),
+    TUESDAY("9am","6pm"),
+    WEDNESDAY("9am","6pm"),
+    THURSDAY("9am","6pm"),
+    FRIDAY("9am","6pm"),
+    SATURDAY("10am","7pm");
+    WeekDay(String startTime, String endTime){
+      this.startTime=startTime;
+      this.endTime=endTime;
+    }
+
+    private String startTime;
+    private String endTime;
+
+    public String getStartTime(){
+        return startTime;
+    }
+
+    public String getEndTime(){
+        return endTime;
+    }
+}
