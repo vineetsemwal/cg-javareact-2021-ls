f57a068a9acf0fa09a4ba6c987d42be3d0327157
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-02 11:08:54 +0530

    jpa demo2 added

---
+++ jpademos/jpademo2/src/main/java/org/cg/apps/jpademo2/entities/Student.java
@@ -0,0 +1,61 @@
+package org.cg.apps.jpademo2.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Table(name="students_data")
+@Entity
+public class Student {
+
+   @GeneratedValue
+    @Id
+    private Long id;
+    private String name;
+    private int age;
+
+    public Student(){
+
+    }
+
+    public Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ jpademos/jpademo2/src/main/java/org/cg/apps/jpademo2/ui/DemoMain.java
@@ -0,0 +1,74 @@
+package org.cg.apps.jpademo2.ui;
+
+import org.cg.apps.jpademo2.entities.Student;
+
+import javax.persistence.*;
+
+public class DemoMain {
+    EntityManager entityManager;
+
+    public static void main(String args[]) throws Exception {
+        DemoMain demo = new DemoMain();
+        demo.start();
+    }
+
+
+    public void start() throws Exception{
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("training-ms");
+        entityManager = emf.createEntityManager();
+        Student mohan = new Student("mohan", 21);
+        add(mohan);
+        Student surya = new Student("surya", 22);
+        add(surya);
+        long suryaId=surya.getId();
+        Student fetched=findById(suryaId);
+        display(fetched);
+
+        Student rohit=new Student("rohit",21);
+        update(rohit);
+
+        entityManager.close();
+
+    }
+
+    void add(Student student) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(student);
+        transaction.commit();
+        System.out.println("student added");
+        display(student);
+    }
+
+    void update(Student student) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        student=entityManager.merge(student);
+        System.out.println("student updated or inserted");
+        display(student);
+        transaction.commit();
+
+    }
+
+    void remove(Student student) {
+        EntityTransaction transaction=entityManager.getTransaction();
+        transaction.begin();
+        entityManager.remove(student);
+        transaction.commit();
+        System.out.println("student removed");
+        display(student);
+    }
+
+    Student findById(long id) {
+        Student student = entityManager.find(Student.class, id);
+        System.out.println("fetch student for id=" + id);
+        return student;
+    }
+
+
+    void display(Student student) {
+        System.out.println("student-" + student.getId() + "-" + student.getName() + "-" + student.getAge());
+    }
+
+
+}
