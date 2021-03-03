e5dfd2573e9a54d35f0af9dea54db81207313673
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-03 10:49:04 +0530

    onetomany one directional

---
+++ jpademos/associations/onetomany/src/main/java/org/cg/apps/jpademo2/entities/Department.java
@@ -0,0 +1,65 @@
+package org.cg.apps.jpademo2.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Table(name ="departments_data")
+@Entity
+public class Department {
+
+    @GeneratedValue
+    @Id
+    private Integer deptId;
+
+    private String name;
+
+    @OneToMany
+    private Set<Employee> employees;
+
+    public Department(){
+
+    }
+
+    public Department(String name,Set<Employee>employees){
+        this.name = name;
+        this.employees=employees;
+    }
+
+    public Integer getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Integer deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(deptId, that.deptId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deptId);
+    }
+}
---
+++ jpademos/associations/onetomany/src/main/java/org/cg/apps/jpademo2/ui/DemoMain.java
@@ -0,0 +1,75 @@
+package org.cg.apps.jpademo2.ui;
+
+import org.cg.apps.jpademo2.entities.Department;
+import org.cg.apps.jpademo2.entities.Employee;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DemoMain {
+    EntityManager entityManager;
+
+    public static void main(String args[]) {
+        DemoMain demo = new DemoMain();
+        demo.start();
+    }
+
+
+    public void start() {
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("training-ms");
+        entityManager=emf.createEntityManager();
+        EntityTransaction transaction=entityManager.getTransaction();
+        transaction.begin();
+        Employee rohit =new Employee("rohit",21);
+        entityManager.persist(rohit);
+
+        Employee arpit =new Employee("arpit",21);
+        entityManager.persist(arpit);
+
+        Set<Employee> devEmployees=new HashSet<>();
+        devEmployees.add(rohit);
+        devEmployees.add(arpit);
+
+        Department dev=new Department("dev",devEmployees);
+        entityManager.persist(dev);
+        Integer devId=dev.getDeptId();
+
+        Employee nisha=new Employee("nisha",21);
+        Employee tanisha=new Employee("tanisha",21);
+        entityManager.persist(nisha);
+        entityManager.persist(tanisha);
+        Set<Employee>testingEmployees=new HashSet<>();
+        testingEmployees.add(nisha);
+        testingEmployees.add(tanisha);
+        Department testing=new Department("testing",testingEmployees);
+        entityManager.persist(testing);
+        Integer testingId=testing.getDeptId();
+        transaction.commit();
+
+        System.out.println("department found");
+        Department devFound=entityManager.find(Department.class,devId);
+        display(devFound);
+        Set<Employee>devFoundEmployees=devFound.getEmployees();
+        displayEmployees(devFoundEmployees);
+        entityManager.close();
+
+    }
+
+    void displayEmployees(Collection<Employee> employees){
+        for(Employee e:employees){
+            display(e);
+        }
+    }
+    void  display(Department department){
+        System.out.println("department-"+department.getDeptId()+"-"+department.getName());
+    }
+
+    void display(Employee employee) {
+        System.out.println("employee-" + employee.getId() + "-" + employee.getName() + "-" + employee.getAge());
+
+    }
+
+
+}
