b3415c5c001e62ceaf08fd2c423fa777b18f1c42
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-11 11:58:30 +0530

    minor change

--- spring/bootrest/schoolmswithoutjpa/src/main/java/com/cg/apps/schoolmswithoutjpa/studentms/controllers/CentralizedExceptionHandler.java
+++ spring/bootrest/schoolmswithoutjpa/src/main/java/com/cg/apps/schoolmswithoutjpa/studentms/controllers/CentralizedExceptionHandler.java
@@ -3,6 +3,8 @@ package com.cg.apps.schoolmswithoutjpa.studentms.controllers;
 import com.cg.apps.schoolmswithoutjpa.studentms.exceptions.InvalidScoreException;
 import com.cg.apps.schoolmswithoutjpa.studentms.exceptions.InvalidStudentNameException;
 import com.cg.apps.schoolmswithoutjpa.studentms.exceptions.StudentNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CentralizedExceptionHandler {
+  private static final Logger Log= LoggerFactory.getLogger(CentralizedExceptionHandler.class);
 
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(StudentNotFoundException.class)
     public String handleStudentNotFound(StudentNotFoundException e){
+        Log.error("exception", e);
         return  e.getMessage();
     }
 
