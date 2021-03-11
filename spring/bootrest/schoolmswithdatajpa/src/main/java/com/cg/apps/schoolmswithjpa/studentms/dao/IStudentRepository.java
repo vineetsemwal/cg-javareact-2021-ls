81db024e3e62e8d239f9663f0d9e0ff0239f9127
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-11 18:22:15 +0530

    data jpa query demo added

--- spring/bootrest/schoolmswithdatajpa/src/main/java/com/cg/apps/schoolmswithjpa/studentms/dao/IStudentRepository.java
+++ spring/bootrest/schoolmswithdatajpa/src/main/java/com/cg/apps/schoolmswithjpa/studentms/dao/IStudentRepository.java
@@ -3,6 +3,8 @@ package com.cg.apps.schoolmswithjpa.studentms.dao;
 
 import com.cg.apps.schoolmswithjpa.studentms.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,11 @@ public interface IStudentRepository extends JpaRepository<Student, Integer> {
     List<Student> findByName(String name);
 
     List<Student> findByScore(int score);
+
+    /**
+     * find students for the provided name where score is greater than provided score
+     *
+     */
+    @Query("from Student where name=:nameArg and score>:scoreArg")
+    List<Student>findByNameWhereScoreGreater(@Param("nameArg") String name,@Param("scoreArg") int score);
 }
