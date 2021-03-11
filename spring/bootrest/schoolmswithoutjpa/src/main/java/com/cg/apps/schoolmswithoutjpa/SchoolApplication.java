a284733691b5ebf845a4ecff29ee2f1956c6566e
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-11 11:53:07 +0530

    logging experiment added

--- spring/bootrest/schoolmswithoutjpa/src/main/java/com/cg/apps/schoolmswithoutjpa/SchoolApplication.java
+++ spring/bootrest/schoolmswithoutjpa/src/main/java/com/cg/apps/schoolmswithoutjpa/SchoolApplication.java
@@ -1,6 +1,8 @@
 package com.cg.apps.schoolmswithoutjpa;
 
 import com.cg.apps.schoolmswithoutjpa.studentms.ui.SchoolUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -8,10 +10,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class SchoolApplication {
 
+    private static final Logger Log= LoggerFactory.getLogger(SchoolApplication.class);
+
     public static void main(String[] args){
        ConfigurableApplicationContext context= SpringApplication.run(SchoolApplication.class, args);
        SchoolUI schoolUI=context.getBean(SchoolUI.class);
        schoolUI.start();
+
+        Log.debug("****i am logged using debug level");
+        Log.info("***i am logged using info level");
+        Log.error("***i am logged using error level");
+
 
     }
 
