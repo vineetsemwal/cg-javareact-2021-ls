20298ce7b9e0979dbd12a242d1aec18053c439e1
vineetsemwal <vineetsemwal82@gmail.com>
2021-03-18 18:17:36 +0530

    mockito with annotations

---
+++ testsdemo/calcydemo/src/test/java/org/cg/apps/calcydemo/CalculatorUnitTest2.java
@@ -0,0 +1,53 @@
+package org.cg.apps.calcydemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculatorUnitTest2 {
+
+
+    Calculator calculator;
+    @Mock
+    Adder adder;
+
+    @Spy
+    @InjectMocks
+    Calculator spy;
+
+
+    @BeforeEach
+    public void setup() {
+        calculator=new Calculator(adder);
+    }
+
+
+    /**
+     * scanario num1=3, num2=5
+     * expectation : result is 8
+     */
+    @Test
+    public void testAdd_2(){
+        Mockito.when(adder.add(3,5)).thenReturn(8);
+        int result=calculator.add(3,5);
+        Assertions.assertEquals(8,result);
+        Mockito.verify(adder).add(3,5);
+    }
+
+    @Test
+    public void testAdd_3(){
+        //Mockito.when(spy.add(4,10)).thenReturn(14);
+        Mockito.doReturn(14).when(spy).add(4,10);
+        int result=spy.addBy10(4);
+        Assertions.assertEquals(14,result);
+    }
+
+
+}
