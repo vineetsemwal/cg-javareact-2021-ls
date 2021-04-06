fc8e7afe5f1eb64c5cb087875738e36d52c3f9db
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-06 17:31:32 +0530

    more js demo

---
+++ clientside/javascript/js2.js
@@ -0,0 +1,20 @@
+
+var user1={
+    name : "arpit",
+    age : 21
+};
+
+
+var user2={
+name : "mohan",
+age:22
+}
+
+function displayUser(user){
+ console.log("user="+user.name+" "+user.age);
+}
+
+displayUser(user1);
+
+displayUser(user2);
+
