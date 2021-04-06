a07a2e206f6882cb5b577954a50d9ffb4ac7b2b5
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-06 19:06:17 +0530

    string demo improved

--- clientside/javascript/js10.js
+++ clientside/javascript/js10.js
@@ -1,9 +1,22 @@
-
+/**
+ * string example
+ */
 let text1='hi, \n how are you';
 let text2=`hi,
 how are you
 i am fine
 `;
 
+
 console.log("text1="+text1);
 console.log("text2="+text2);
+
+
+let result=20;
+
+let resultText1="result is "+result+" bye";
+let resultText2=`result is ${result} bye`;
+
+console.log("resulttext1="+resultText1);
+
+console.log("resulttext2="+resultText2);
