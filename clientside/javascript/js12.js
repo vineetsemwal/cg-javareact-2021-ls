666c761b6dbc2ecc5660f65ccb648669d7fdf732
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-06 19:22:33 +0530

    object destructing demo added

---
+++ clientside/javascript/js12.js
@@ -0,0 +1,16 @@
+/**
+ * object destructing
+ */
+
+let user={name:'nisha', age:21};
+
+let name1=user.name;
+
+let age1=user.age;
+
+console.log(name1 +" "+age1);
+
+
+let {name:name2,age:age2 }=user;// object destructuring
+
+console.log(name2 +" "+age2);
