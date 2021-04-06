eb5777d9ca488e6c64863f638f80e0f42b7e8581
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-06 17:13:32 +0530

    function demo added

--- clientside/javascript/js1.js
+++ clientside/javascript/js1.js
@@ -1,3 +1,9 @@
 
-var username="arpit";
-console.log("name is "+username);
+function add(num1, num2){
+ var total= num1+num2;
+ return total;
+}
+
+var result=add(1,2);
+
+console.log("result="+result);
