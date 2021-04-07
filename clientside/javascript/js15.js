d473ef41aeb045d8a8a2251b4718cca8530c4a5b
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-07 11:36:19 +0530

    array desctructuring demo improved

--- clientside/javascript/js15.js
+++ clientside/javascript/js15.js
@@ -8,5 +8,12 @@ let numbers=[10,20,30,40];
 //let num2=numbers[1];
 
 let [num1, num2, num3, num4]=numbers;
-
 console.log(num1 +" "+num2+" "+num3+" "+num4);
+
+let [num11,num22,...remaining]=numbers;
+
+console.log(num11+" "+num22);
+
+for(let num of remaining){
+    console.log(num);
+}
