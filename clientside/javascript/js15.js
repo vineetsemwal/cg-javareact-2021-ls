5bfb46e2eebe5a05e4d3e31cffc316e838f809bd
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-07 11:32:23 +0530

    array desctructuring demo added

---
+++ clientside/javascript/js15.js
@@ -0,0 +1,12 @@
+/**
+ * array destructuring
+ */
+
+let numbers=[10,20,30,40];
+
+//let num1=numbers[0];
+//let num2=numbers[1];
+
+let [num1, num2, num3, num4]=numbers;
+
+console.log(num1 +" "+num2+" "+num3+" "+num4);
