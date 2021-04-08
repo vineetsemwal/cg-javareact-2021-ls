6d6099516c9e17c5168add07bfb6b8f0b15ab01f
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-08 17:02:03 +0530

    comments added

--- clientside/reactjs/app1/src/ClickCounter.js
+++ clientside/reactjs/app1/src/ClickCounter.js
@@ -3,18 +3,16 @@ import React from 'react';
 
 
 class ClickCounter extends Component {
-
     
-
     constructor(props){
         super(props);
-        this.state={counter:0};
+        this.state={counter:0};// initial state
     }    
 
 
     increment() {
   
-      this.setState({counter:this.state.counter+1});
+      this.setState({counter:this.state.counter+1}); //state update
     }
 
     render() {
