a7f8f4ff990a7ba8a68a48309ad0881775feee7f
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-08 17:32:03 +0530

    minor formatting

--- clientside/reactjs/app1/src/UsersTabular.js
+++ clientside/reactjs/app1/src/UsersTabular.js
@@ -3,11 +3,13 @@ import UserInfoTD from "./UserInfoTD";
 function UsersTabular({users}) {
   
 
-  return <table border="1">{
+  return <table border="1">
+    {
     
     users.map(user=><tr key={user.id}> <UserInfoTD  user={user} />  </tr>)
     
-    }</table>;
+    }
+    </table>;
 }
 
 
