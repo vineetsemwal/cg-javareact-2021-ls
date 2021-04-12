cc9a98c08405cdc17a76144f7732f8283a5abc82
vineetsemwal <vineetsemwal82@gmail.com>
2021-04-12 17:57:49 +0530

    minor improvement in count functional demo example

--- clientside/reactjs/statefulfunctional/src/ClickCounter.js
+++ clientside/reactjs/statefulfunctional/src/ClickCounter.js
@@ -6,8 +6,10 @@ export default function ClickCounter() {
 
      const incrementHandler=()=>{
          let newState=counter+1;
-         const stateFun=prevCount=>prevCount+1;
-         setCounter(stateFun);
+         setCounter(newState);
+         // another way pass the function in setCounter which will use previous state to create new state
+         // const stateFun=prevCount=>prevCount+1;
+         //setCounter(stateFun);
      }
 
     return (
